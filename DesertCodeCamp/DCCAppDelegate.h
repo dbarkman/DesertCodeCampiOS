0bc06d6192d7fac04328e935b2c33e21aa4c7ee3
David Barkman <David@Davids-MacBook.local>
2012-10-21 23:06:53 -0700

    Initial Commit

---
+++ DesertCodeCamp/DCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCCAppDelegate.h
+//  DesertCodeCamp
+//
+//  Created by David Barkman on 10/21/12.
+//  Copyright (c) 2012 RealSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DesertCodeCamp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DesertCodeCamp
+//
+//  Created by David Barkman on 10/21/12.
+//  Copyright (c) 2012 RealSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCCAppDelegate class]));
+	}
+}
